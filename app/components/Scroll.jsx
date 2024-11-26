802e0fc792ed435cd789ca624ba31b6c2cefc6ff
viveksharma19110 <viveksharma19110@gmail.com>
2024-11-26 15:19:03 +0530

    favicon added and Discover added

--- app/components/Scroll.jsx
+++ app/components/Scroll.jsx
@@ -6,7 +6,8 @@ const Scroll = ({ data }) => {
   return (
     <div className="flex justify-center py-8">
       <div className="bg-white bg-opacity-80 rounded-lg shadow-lg w-full max-w-6xl h-[80vh] overflow-auto no-scrollbar">
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 p-8">
+        <h1 className='text-black font-semibold ml-4 mt-2 '>DISCOVER</h1>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 p-4">
           <Cards data={data} />
         </div>
       </div>
